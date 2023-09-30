64dcbce9c8507463be0bb70092567781e56028b4
Mahitha Reddy <kmahitha1998@gmail.com>
2023-09-30 11:31:19 -0500

    commiting code and runner class

---
+++ MultiplicationTableRunner.java
@@ -0,0 +1,12 @@
+package com.mahitha.firstjavaproject;
+
+public class MultiplicationTableRunner {
+
+	public static void main(String[] args) {
+		MultiplicationTable table = new MultiplicationTable();
+		table.print(15);
+		//table.print();
+		//table.print(15,1,20);
+	}
+
+}
